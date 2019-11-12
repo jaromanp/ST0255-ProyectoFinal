1093d2a84a163ec5469961650379cd39474ee0a9
Alejandro <jaromanp@eafit.edu.co>
2019-11-11 23:22:36 -0500

    Se agrego la primera vista

--- web/web/urls.py
+++ web/web/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+#url Producto:crear_producto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('productos/', include(('apps.Producto.urls','Producto')))
 ]
