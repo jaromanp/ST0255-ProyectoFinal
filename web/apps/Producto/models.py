39d82a669d39274d674d9254626d3c95ed09264b
Alejandro <jaromanp@eafit.edu.co>
2019-11-11 22:46:13 -0500

    Configuracion URLS

--- web/apps/Producto/models.py
+++ web/apps/Producto/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Producto(models.Model):
-    id = models.AutoFiel(primary_key = True)
-    nombre = models.CharField(max_lenght = 200, blank = False, null = False)
+class Product(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 200, blank = False, null = False)
+
+
+def __str__(self):
+    return self.nombre
