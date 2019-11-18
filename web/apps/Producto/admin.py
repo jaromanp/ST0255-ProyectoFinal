98d052105c171e73985d6222f4c1b45cfa462cc4
Alejandro <jaromanp@eafit.edu.co>
2019-11-18 12:02:54 -0500

    Adicion del modelo de datos

--- web/apps/Producto/admin.py
+++ web/apps/Producto/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Product
+from .models import Usuario
+from .models import Comentario
 
 admin.site.register(Product)
+admin.site.register(Usuario)
+admin.site.register(Comentario)
+
--- web/apps/Producto/models.py
+++ web/apps/Producto/models.py
@@ -2,9 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 200, blank = False, null = False)
+    id_product = models.AutoField(primary_key = True)
+    nombreP = models.CharField(max_length = 200, blank = False, null = False)
+    categoria = models.CharField(max_length = 50, blank = False, null = False, default = "default")
+    precio = models.IntegerField(default = 0)
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key = True) 
+    nombreUser = models.CharField(max_length = 20, blank = False, null = False)
+    edad = models.IntegerField(null = False)
+    ciudad = models.CharField(max_length = 30, blank = False, null = False)
+    direccion = models.CharField(max_length = 50, blank = False, null = False)
+    email = models.EmailField()
 
 
-def __str__(self):
-    return self.nombre
+class Comentario(models.Model):
+    id_comentario = models.AutoField(primary_key = True)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    comentario = models.TextField(max_length = 150)
+    pub_date = models.DateField()
+
+
+
+
+
+
+
+
